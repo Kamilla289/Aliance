0cfc8344d73239ddbff0bda85cf3306111570aa5
Kamilla289 <kami.rivay56@gmail.com>
2024-09-09 20:31:52 +0500

    fix: handler.php and tg

--- handler.php
+++ handler.php
@@ -1,5 +1,34 @@
 <?php
-$user_name = $_POST["username"];
+$user_name = htmlspecialchars($_POST["username"]);
 $user_phone = $_POST["userphone"];
+
+
+$token = "6632009151:AAEvgwc5QOtxEzuE4XlK91GpiFufzCSgAZs";
+$chat_id = "-4550241662";
+
+$formData = array(
+  "Клиент: " => $user_name,
+  "Телефон: " => $user_phone
+);
+
+
+if(!isset($text))  //Добавила, так как text был необъявлен и выдавал ошибку при отправке формы
+    $text = '';    //Добавила, так как text был необъявлен и выдавал ошибку при отправке формы
+
+echo $text;        //Добавила, так как text был необъявлен и выдавал ошибку при отправке формы
+
+
+foreach($formData as $key => $value) {
+  $text .= $key . "<b>" . urlencode($value) . "</b>" . "%0A";
+}
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$text}&parse_mode=html", "r");
+
+if ($sendToTelegram) {
+  echo "Success";
+} else {
+  echo "Error";
+}
+
 echo "Привет, " . $user_name . "<br>";
 echo "Ваш телефон: <b>" . $user_phone . "</b>";
