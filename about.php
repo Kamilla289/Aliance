7e4b8195a1d7baa2a811a6b1cd57e6068f101c20
Kamilla289 <kami.rivay56@gmail.com>
2024-09-21 20:06:14 +0500

    fix: about

--- about.php
+++ about.php
@@ -1,4 +1,6 @@
   <?php 
   $page_title = 'О компании';
   include_once('header-page.php');?>
+  <?php include_once('./template-parts/founder.php');?>
+  <?php include_once('./template-parts/clients.php');?>
   <?php include_once('footer.php');?>
